1facfb0dfb5daabb4a6aee7be80a8a682cc7388c
MARIA EDUARDA FONTINELE DE CASTRO <maria.castro@alunoescolassociais.org.br>
2024-07-04 10:18:26 -0300

    Criamos a estrutura JavaScript.

---
+++ controller.js
@@ -0,0 +1,27 @@
+// VALIDAR ACESSO EM TELA DE LOGIN
+ 
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+ 
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos");
+    }else{
+        //alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html';
+    }
+}
+
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        document.getElementById('nomeUser').value = "";
+    }else{
+        alert("Favor informar o nome para cadastro");
+    }
+}
